69352fcef29c2083016968ca38c1270d272dc30d
Angus Wai <anguswai1999@gmail.com>
2020-11-13 16:34:03 -0500

    basic input and output test

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,22 @@
+/**
+ * @author Angus Wai, Student ID: 100719558
+ * @date November 13, 2020
+ * @version 0.0.1
+ */
+
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = null;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You Entered: %s", name);
     }
 }
